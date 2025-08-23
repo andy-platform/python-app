53da42055c4a2e7552edcf90f4c2c4ee1a9ac622
Andy Nzeka <andynze4@gmail.com>
2025-08-23 14:51:19 -0500

    Message': '🚀 Deployment successful

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ def info():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
         'hostname': socket.gethostname(),
-        'message': 'Deployment successful! Your Python app is alive and running 🚀 🚀 🚀 🚀 🚀 🚀 .',
+        'message': 'Deployment successful! Your Python app is alive and running!',
         'deployed_on': 'kubernetes'
     })
 
